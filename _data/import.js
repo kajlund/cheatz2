c9d389719c9309b4517faf9186dca449663792d1
Kaj Lund <kaj.lund@gmail.com>
2023-01-07 00:05:06 +0200

    Moved imports to same file added user import

--- _data/import.js
+++ _data/import.js
@@ -1,12 +1,47 @@
-/* eslint-disable no-console */
-const caches = require('./caches.json')
-
 const { PrismaClient } = require('@prisma/client')
+const { hash } = require('bcrypt')
+
+const cnf = require('../src/config')
+const caches = require('./caches.json')
+const municipalities = require('./municipalities.json')
+const users = require('./users.json')
+
 const prisma = new PrismaClient()
+
+const addUsers = async () => {
+  // delete all
+  await prisma.user.deleteMany({})
+
+  users.forEach(async (user) => {
+    user.password = await hash(user.password, cnf.saltRounds)
+    await prisma.user.create({
+      data: user,
+    })
+  })
+}
+
+const addMunicipalities = async () => {
+  // delete all
+  await prisma.municipality.deleteMany({})
+
+  for (const key in municipalities) {
+    let nameSv = municipalities[key].KUNTANIMISV
+    let name = municipalities[key].KUNTANIMIFI
+    if (nameSv !== name) {
+      name += `/${nameSv}`
+    }
+    await prisma.municipality.create({
+      data: {
+        code: parseInt(key, 10),
+        name,
+      },
+    })
+  }
+}
 
 const addCaches = async () => {
   // Delete all
-  await prisma.cacheComment.deleteMany({})
+  // should cascade -> await prisma.cacheComment.deleteMany({})
   await prisma.geocache.deleteMany({})
   const user = await prisma.user.findFirst({ where: { email: 'kaj.lund@gmail.com' } })
 
@@ -50,4 +85,10 @@ const addCaches = async () => {
   }
 }
 
-addCaches()
+const importAll = async () => {
+  await addUsers()
+  await addMunicipalities()
+  await addCaches()
+}
+
+importAll()
