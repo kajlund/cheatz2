48ff517cae2e347aa749cf8ac4f968102e29f548
Kaj Lund <kaj.lund@gmail.com>
2023-01-06 19:25:31 +0200

    Simplified config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 const dotenv = require('dotenv')
+const { merge } = require('lodash')
 
 // Load environment variables BEFORE setting up config
 dotenv.config({ path: path.join(__dirname, '..', '.env') })
@@ -12,91 +13,49 @@ const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS) || 12
 const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET
 const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET
 
-const config = {
-  development: {
-    db: {
-      url: DATABASE_URL,
-    },
-    port: PORT,
-    nodeEnv: ENV,
-    saltRounds: SALT_ROUNDS,
-    jwtAccessTokenExpiresIn: 3600 * 24, // 24hr
-    jwtAccessTokenSecret: JWT_ACCESS_TOKEN_SECRET,
-    jwtRefreshTokenSecret: JWT_REFRESH_TOKEN_SECRET,
-    log: {
-      enabled: true,
-      level: 'trace',
-    },
-    mail: {
-      connection: process.env.MAIL_CONNECTION || 'smtp',
-      from: process.env.MAIL_FROM,
-      smtp: {
-        driver: 'smtp',
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT,
-        auth: {
-          user: process.env.MAIL_USERNAME,
-          pass: process.env.MAIL_PASSWORD,
-        },
-      },
-    },
+const baseConfig = {
+  db: {
+    url: DATABASE_URL,
   },
-  production: {
-    db: {
-      url: DATABASE_URL,
-    },
-    port: PORT,
-    nodeEnv: ENV,
-    saltRounds: SALT_ROUNDS,
-    jwtAccessTokenExpiresIn: 3600, // 1hr
-    jwtAccessTokenSecret: JWT_ACCESS_TOKEN_SECRET,
-    jwtRefreshTokenSecret: JWT_REFRESH_TOKEN_SECRET,
-    log: {
-      enabled: true,
-      level: 'info',
-    },
-    mail: {
-      connection: process.env.MAIL_CONNECTION || 'smtp',
-      from: process.env.MAIL_FROM,
-      smtp: {
-        driver: 'smtp',
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT,
-        auth: {
-          user: process.env.MAIL_USERNAME,
-          pass: process.env.MAIL_PASSWORD,
-        },
-      },
-    },
+  port: PORT,
+  nodeEnv: ENV,
+  saltRounds: SALT_ROUNDS,
+  jwtAccessTokenExpiresIn: 3600 * 24, // 24hr
+  jwtAccessTokenSecret: JWT_ACCESS_TOKEN_SECRET,
+  jwtRefreshTokenSecret: JWT_REFRESH_TOKEN_SECRET,
+  log: {
+    enabled: true,
+    level: 'trace',
   },
-  test: {
-    db: {
-      url: DATABASE_URL,
-    },
-    port: PORT,
-    nodeEnv: ENV,
-    saltRounds: SALT_ROUNDS,
-    jwtAccessTokenExpiresIn: 3600, // 1hr
-    jwtAccessTokenSecret: JWT_ACCESS_TOKEN_SECRET,
-    jwtRefreshTokenSecret: JWT_REFRESH_TOKEN_SECRET,
-    log: {
-      enabled: true,
-      level: 'trace',
-    },
-    mail: {
-      connection: process.env.MAIL_CONNECTION || 'smtp',
-      from: process.env.MAIL_FROM,
-      smtp: {
-        driver: 'smtp',
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT,
-        auth: {
-          user: process.env.MAIL_USERNAME,
-          pass: process.env.MAIL_PASSWORD,
-        },
+  mail: {
+    connection: process.env.MAIL_CONNECTION || 'smtp',
+    from: process.env.MAIL_FROM,
+    smtp: {
+      driver: 'smtp',
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
       },
     },
   },
 }
 
-module.exports = config[ENV]
+let envConfig = {}
+
+if (ENV === 'production') {
+  envConfig.log = {
+    enabled: true,
+    level: 'info',
+  }
+}
+
+if (ENV === 'testing') {
+  envConfig.log = {
+    enabled: false,
+    level: 'info',
+  }
+}
+
+module.exports = merge(baseConfig, envConfig)
